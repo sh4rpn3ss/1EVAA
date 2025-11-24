75fc8cd25ff5fd5b56d307393de855da3a0d62be
sh4rpn3ss <marcojb2006.mjb@gmail.com>
2025-11-24 10:22:20 +0100

    CONSTANTES

---
+++ src/main/java/org/example/constantes.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public final class constantes {
+            public static final String AGENDA = "\n=== AGENDA ===";
+            public static final String LISTAR = "1. Listar";
+            public static final String DIA = "2. Buscar por día";
+            public static final String NOMBRE = "3. Buscar por nombre";
+            public static final String AÑADIR = "4. Añadir";
+            public static final String ELIMINAR = "5. Eliminar";
+            public static final String SALR = "6. Salir";
+            public static final String OPCION = "Opción: ";
+            public static final String VACIO = "Vacío.";
+            public static final String SINRESULTADOS = "Sin resultados.";
+            public static final String NOENCONTRADO = "No encontrado.";
+            public static final String NOMBREAELIMINAR = "Nombre a eliminar: ";
+            public static final String NOMBREE = "Nombre: ";
+            public static final String NOMBREEE = "Nombre: ";
+            public static final String DIAA = "Día: ";
+            public static final String DIAAA = "Día: ";
+            public static final String MES = "Mes: ";
+}
